0f4c39e70ac05b13b7f40e44984788b25665c4ac
Goldie5fnf <82838084+Goldie5fnf@users.noreply.github.com>
2023-04-15 13:48:49 +0300

    Update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -114,11 +114,11 @@ class FreeplayState extends MusicBeatState
 
 		var swag:Alphabet = new Alphabet(1, 0, "swag");
 
+		super.create();
+
                 #if mobile
                 addVirtualPad(LEFT_FULL, A_B);
                 #end
-
-		super.create();
 	}
 
 	public function addSong(songName:String, weekNum:Int, songCharacter:String)
